5531c0c37b0a2dda7daffa75b34a0ddb24218ce9
AqSlz <44170778+AqSlz@users.noreply.github.com>
2020-02-01 20:59:33 -0500

    first commit for branch

---
+++ backend/routes/animationTest.js
@@ -0,0 +1,40 @@
+import React, { useState, useEffect } from 'react';
+import { Animated, Text, View } from 'react-native';
+
+const FadeInView = (props) => {
+  const [fadeAnim] = useState(new Animated.Value(0))  // Initial value for opacity: 0
+
+  React.useEffect(() => {
+    Animated.timing(
+        // Animate value over time
+        this.state.fadeAnim, // The value to drive
+        {
+          toValue: 1, // Animate to final value of 1
+        },
+      ).start(); // Start the animation
+  }, [])
+
+ 
+
+  return (
+    <Animated.View                 // Special animatable View
+      style={{
+        ...props.style,
+        opacity: fadeAnim,         // Bind opacity to animated value
+      }}
+    >
+      {props.children}
+    </Animated.View>
+  );
+}
+
+// You can then use your `FadeInView` in place of a `View` in your components:
+export default why = () => {
+  return (
+    <View style={{flex: 1, alignItems: 'center', justifyContent: 'center'}}>
+      <FadeInView style={{width: 250, height: 50, backgroundColor: 'powderblue'}}>
+        <Text style={{fontSize: 28, textAlign: 'center', margin: 10}}>Fading in</Text>
+      </FadeInView>
+    </View>
+  )
+}
